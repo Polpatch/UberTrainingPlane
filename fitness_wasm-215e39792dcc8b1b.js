efb24ed666ceda305059e8f4fb2a46478d958c81
Polpatch <Polpatch@users.noreply.github.com>
2026-06-17 00:52:41 +0000

    deploy: b5736f390594201b8c06ada6c7af0cc9b135acdd

--- fitness_wasm-215e39792dcc8b1b.js
+++ fitness_wasm-215e39792dcc8b1b.js
@@ -653,7 +653,7 @@ function __wbg_get_imports() {
             return ret;
         },
         __wbindgen_cast_0000000000000003: function(arg0, arg1) {
-            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 633, ret: Unit, inner_ret: Some(Unit) }, mutable: false }) -> Externref`.
+            // Cast intrinsic for `Closure(Closure { owned: true, function: Function { arguments: [], shim_idx: 575, ret: Unit, inner_ret: Some(Unit) }, mutable: false }) -> Externref`.
             const ret = makeClosure(arg0, arg1, wasm_bindgen__convert__closures_____invoke__hed09327cc67ec7f8);
             return ret;
         },
